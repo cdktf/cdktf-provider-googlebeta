983df203e2356dd395d3c2d7d157274063c54795
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2022-11-03 01:32:09 +0100

    chore(deps): upgrade dependencies (#245)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/cdktf/cdktf-provider-googlebeta/actions/runs/3382067674
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*

--- scripts/check-for-upgrades.js
+++ scripts/check-for-upgrades.js
@@ -11,7 +11,7 @@ const actions = require("@actions/core");
 
 // set by the projen file that generates this script
 const CONSTRAINT = "~> 4.17";
-const FQ_PROVIDER_NAME = "google-beta";
+const FQ_PROVIDER_NAME = "hashicorp/google-beta";
 
 (async function main() {
   if (await newerMatchingProviderVersionExists()) {
