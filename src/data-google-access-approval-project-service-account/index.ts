c6bdc5ec9599c3f303435112079019185142ed18
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-06 15:47:48 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-google-access-approval-project-service-account/index.ts
+++ src/data-google-access-approval-project-service-account/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/google-beta/6.14.1/docs/data-sources/google_access_approval_project_service_account
 // generated from terraform resource schema
 
